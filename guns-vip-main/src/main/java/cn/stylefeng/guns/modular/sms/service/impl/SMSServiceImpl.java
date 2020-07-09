549362fb6d2d977f5ee53839a7a287cd4fbedfae
chujianqiao <1172544385@qq.com>
2020-07-09 11:23:47 +0800

    20200709 CHU

--- guns-vip-main/src/main/java/cn/stylefeng/guns/modular/sms/service/impl/SMSServiceImpl.java
+++ guns-vip-main/src/main/java/cn/stylefeng/guns/modular/sms/service/impl/SMSServiceImpl.java
@@ -38,8 +38,6 @@ public class SMSServiceImpl implements SMSService {
             final String product = "Dysmsapi";//短信API产品名称（短信产品名固定，无需修改）
             final String domain = "dysmsapi.aliyuncs.com";//短信API产品域名（接口地址固定，无需修改）
             //替换成你的AK
-            //final String accessKeyId = "LTAI4G8fGDZuKV1T88CedR14";//你的accessKeyId,参考本文档步骤2
-            //final String accessKeySecret = "u1jvMhv6iaG53gpDu5pt6moNq3rflv";//你的accessKeySecret，参考本文档步骤2
             //初始化ascClient,暂时不支持多region（请勿修改）
             IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId,
                     accessKeySecret);
